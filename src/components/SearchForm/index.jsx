226624642c79337c3deb8ed6e75596f9273d16e8
Maxim Noritsyn <maximnoritsyn@gmail.com>
2024-04-28 14:14:39 +0300

    deploy fix

--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useMovies } from '../../context/Movies/index';
 import css from './index.module.css'
 
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -9,7 +9,7 @@ export const HomePage = () => {
 
   useEffect(() => {
     setTrandingMovies()
-  }, []);
+  }, [setTrandingMovies]);
 
 
   return (
